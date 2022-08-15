b35cf590bfbbf5f6019ee78b1913fff4698cb7ca
clybowl <4941@holbertonschool.com>
2022-08-15 15:31:02 -0700

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,27 @@
 *
 */
 
-void insertion_sort_list(listint_t **list)
+void selection_sort(int *array, size_t size)
 {
-	
+	size_t temp, step, mindex, i;
+
+	if (array == NULL)
+		return;
+
+	for (step = 0; step < size - 1; step++)
+	{
+		mindex = step;
+		for (i = step + 1; i < size; i++)
+		{
+			if (array[i] < array[mindex])
+				mindex = i;
+		}
+
+		temp = array[step];
+		array[step] = array[mindex];
+		array[mindex] = temp;
+
+	if (step != mindex)
+		print_array(array, size);
+	}
 }
