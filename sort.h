f9fe04ab396df7ea7bd10ca619fff4c1a22457f7
clybowl <4941@holbertonschool.com>
2022-08-16 10:36:58 -0700

    header betty update

--- sort.h
+++ sort.h
@@ -28,7 +28,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void quick_sort(int *array, size_t size);
 void quickSort(int *array, int low, int high, size_t size);
-void swap(int *array,int *a, int *b, size_t size);
+void swap(int *array, int *a, int *b, size_t size);
 int partition(int *array, int start, int end, size_t size);
 
 #endif
