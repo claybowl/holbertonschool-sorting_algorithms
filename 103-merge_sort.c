7cc1001893497aea8303fe6cdfbbb36aef047e11
claybowl <claybowlproductions@gmail.com>
2023-01-18 20:11:51 -0800

    task 0

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,12 +1,12 @@
 #include "sort.h"
 
 /**
- *print_merge - print and merge
- *@array: random number array
- *@p1: changer 1
- *@p2: changer 2
- *@size: array size
- *Return: void
+ * print_merge - print and merge
+ * @array: random number array
+ * @p1: changer 1
+ * @p2: changer 2
+ * @size: array size
+ * Return: void
  */
 void print_merge(int *array, int *p1, int *p2, size_t size)
 {
@@ -51,10 +51,10 @@ void print_merge(int *array, int *p1, int *p2, size_t size)
 }
 
 /**
- *merge_sort - merge sort
- *@array: random number array
- *@size: array size
- *Return: void
+ * merge_sort - merge sort
+ * @array: random number array
+ * @size: array size
+ * Return: void
  */
 void merge_sort(int *array, size_t size)
 {
