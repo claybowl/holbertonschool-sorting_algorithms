ec94dc3f2df01c2d34eadcd46f66474922578f1f
claybowl <claybowlproductions@gmail.com>
2023-02-22 19:27:13 -0600

    GOT IT

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -7,39 +7,47 @@
 #define WIDTH 3
 
 /**
+ * radix_sort - sorts array of int in ascending order using Radix sort
  * @array: array to be sorted.
- *
  * @size: Number of elements in the array
- * 
- * Return: Array sorted by Redix (LSD) algorithm.
+ *
+ * Return: void.
  */
 void radix_sort(int *array, size_t size)
 {
 	/* Defines variables for loops */
-	size_t i, j, k, m, p, w = WIDTH, d = 1;
+	size_t i, j, k, p, w = WIDTH, d = 1;
 	/* Create our temporary array */
-	int *temp = malloc(sizeof(int) * size);
+	int *temp = NULL;
+
+	if (!array || size < 2)
+		return;
+
+	temp = malloc(sizeof(int) * size);
 	if (!temp)
 		return;
 
 	/* Loops through digit places. Starts with least significant (right) */
 	for (i = 0; i < w; i++, d *= BASE)
 	{
-		for (j = 0; j < BASE; j++)
+		int count[BASE] = {0};
+
+		for (j = 0; j < size; j++)
+			count[(array[j] / d) % BASE]++;
+
+		for (j = 1; j < BASE; j++)
+			count[j] += count[j - 1];
+
+		for (j = size - 1; (int)j >= 0; j--)
 		{
-			m = 0;
-			int start = j * BASE;
-			for (k = 0; k < size; k++)
-			{
-				p = array[k] / d % BASE;
-				if (p == j)
-					temp[m++] = array[k];
-			}
-			int q = start;
-			for (k = 0; k < m; k++, q += BASE)
-				array[q] = temp[k];
+			p = (array[j] / d) % BASE;
+			temp[count[p] - 1] = array[j];
+			count[p]--;
 		}
-		printf("Pass %ld: ", i + 1);
+
+		for (k = 0; k < size; k++)
+			array[k] = temp[k];
+
 		print_array(array, size);
 	}
 
