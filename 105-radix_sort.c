230834eda79f27bb82309c76ce1a88e8362ce8c3
claybowl <claybowlproductions@gmail.com>
2023-02-22 20:34:56 -0600

    sweet baby jesus, did I crack it?

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -10,46 +10,61 @@
  * radix_sort - sorts array of int in ascending order using Radix sort
  * @array: array to be sorted.
  * @size: Number of elements in the array
+ * @d: digit to be sorted.
+ *
+ * Return: void.
+ */
+void counting_sort(int *array, size_t size, int d)
+{
+	int count[BASE] = {0};
+	size_t i, j;
+	int *output = malloc(sizeof(int) * size);
+
+	if (!output)
+		return;
+
+	for (i = 0; i < size; i++)
+		count[(array[i] / d) % BASE]++;
+
+	for (i = 1; i < BASE; i++)
+		count[i] += count[i - 1];
+
+	for (j = size - 1; (int)j >= 0; j--)
+	{
+		output[count[(array[j] / d) % BASE] - 1] = array[j];
+		count[(array[j] / d) % BASE]--;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+
+	free(output);
+}
+
+/**
+ * radix_sort - sorts array of int in ascending order using Radix sort
+ * @array: array to be sorted.
+ * @size: Number of elements in the array
  *
  * Return: void.
  */
 void radix_sort(int *array, size_t size)
 {
-	/* Defines variables for loops */
-	size_t i, j, k, p, w = WIDTH, d = 1;
-	/* Create our temporary array */
-	int *temp = NULL;
+	size_t i;
+	int max_value = 0;
+	int exp = 1;
 
 	if (!array || size < 2)
 		return;
 
-	temp = malloc(sizeof(int) * size);
-	if (!temp)
-		return;
+	for (i = 0; i < size; i++)
+		if (array[i] > max_value)
+			max_value = array[i];
 
-	/* Loops through digit places. Starts with least significant (right) */
-	for (i = 0; i < w; i++, d *= BASE)
+	while (max_value / exp > 0)
 	{
-		int count[BASE] = {0};
-
-		for (j = 0; j < size; j++)
-			count[(array[j] / d) % BASE]++;
-
-		for (j = 1; j < BASE; j++)
-			count[j] += count[j - 1];
-
-		for (j = size - 1; (int)j >= 0; j--)
-		{
-			p = (array[j] / d) % BASE;
-			temp[count[p] - 1] = array[j];
-			count[p]--;
-		}
-
-		for (k = 0; k < size; k++)
-			array[k] = temp[k];
-
+		counting_sort(array, size, exp);
 		print_array(array, size);
+		exp *= BASE;
 	}
-
-	free(temp);
 }
